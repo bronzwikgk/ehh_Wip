6ded5066bbce553f076bfd2b2e277991d6490a3e
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-18 15:50:40 +0530

    u

--- js/ehhGet.js
+++ js/ehhGet.js
@@ -2,47 +2,59 @@ let i = 0;
 nodeOut = {};
 console.log(i, nodeOut); i++;
 output = {};
-let entity = "h1";
-var entityKeysReq = {
+let entity = "toggle";
+var request = {
     'request' : {
-        'localName': "",
-        'childNodes': "",
-        'tagName': "",
-        'children': "",
-        'attributes': "",
-        'nodeName': "",
-        'name': "",
-        'nodeType': "",
-        'style': "",
-        'id': ""
+        'localName': "get",
+        'tagName': "get",
+        'children': "get",
+        'attributes': "get",
+        'nodeName': "get",
+        "nodeValue":"",
+        'name': "get",
+        'nodeType': "get",
+        'style': "get",
+        'id': "get"
     },
     'options': {
         'values': true,
-        'ignore': ['function']
+        'ignore': ['function'],
+        'rules': {
+            "get&ApplyRule1": 'getNameKey',
+            "getNonEmptyValues": 'ifvaluenotblank'
+        }
     }
 }
+
 
 
 tmp = {};
 var temp = getElement(entity, tmp);
 console.log("temp",temp);
-
-
-
-var output = getKeys(entity, entityKeysReq, output, true);
+var output = processRequest(entity, request, output, true);
 console.log(output);
 
 
-function getKeys(entity, entityKeysReq, output, values) {
-    if (!response) { var response = new Map(); }
-    
+function processRequest(entity, request, output, values) {
+    if (!response) { var response = new Map(); } 
     var eip = document.getElementsByTagName(entity)[0];
-    for(key in entityKeysReq.request){
-//console.log(key);
+    for (key in request.request){
+            //console.log(key);
         if (eip[key]){
             if(values === true){
-               // console.log(key, eip[key]);
-                response.set(key, eip[key]);
+               
+                if (eip[key].hasOwnProperty(key) || typeof eip[key] === 'object' || typeof eip[key] === 'function') {
+                    
+                    console.log(key, eip[key]);
+                    response = conductIteration(eip[key]);
+
+                    
+                    //response.set(key, eip[key]);    
+                } else {
+                    // console.log(key, eip[key]);
+                    response.set(key, eip[key]);    
+                }
+                
             } else {
                 response.set(key);
    
@@ -51,16 +63,9 @@ function getKeys(entity, entityKeysReq, output, values) {
         }
     }
 
-
 return response;
 }
 
-
-function getValues() { 
-
-
-
-}
 
 
 function getElement(entity, output) {
@@ -79,7 +84,10 @@ function conductIteration(entity, output, input, sw, keytoFind ,itemLength, iter
     if (entity) {
         if (getEntityType(entity).includes("HTML")) {
             console.log(i, "entity is ", getEntityType(entity)); i++;
+
             var response = iterateObj(entity, output, false, sw);  
+
+
         } else if (getEntityType(entity).includes("List")) {
             console.log("Listentity is ", getEntityType(entity));
             iterateArr(entity);
@@ -109,15 +117,15 @@ function iterateArr(arr,output) {
 
 function iterateObj(obj, nodeOut, recurse,nextSw) {
     sw = arguments.callee.name;
-    console.log(i, sw, entity, getEntityType(entity), entity.nodeType); i++;
+    console.log(i, sw, obj, getEntityType(obj), obj.nodeType); i++;
     for (let key in obj) {
         if (obj[key]) {
            // nodeOut[key] = obj[key];
             //console.log(i, key, obj[key]); i++;
             if (obj.hasOwnProperty(key) || typeof obj[key] === 'object' || typeof obj[key] === 'function') {//In case we need to recurse
-              //  console.log("obj found >>", key, obj[key], getEntityType(obj[key]), obj[key].nodeType,typeof obj[key]);
+               // console.log("obj found >>", key, obj[key], getEntityType(obj[key]), obj[key].nodeType,typeof obj[key]);
                 //iterateEntity(obj, obj[key], nodeOut);
-               nodeOut[key] = obj[key];
+              // nodeOut[key] = obj[key];
             } else {
                 nodeOut[key] = obj[key];
               // console.log(i, key, obj[key]); i++;
