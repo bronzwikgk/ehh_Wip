c6f023619d01560f84864d8c81cb2227409f0f08
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-20 06:26:51 +0530

    u

--- js/ehhLocationRouter.js
+++ js/ehhLocationRouter.js
@@ -29,9 +29,13 @@ function onLocationChange(e) {
 
 function conductRoute(route){
     console.log("route",route);
+    
     if(route.state && route.expires_in && route.token_type){
         console.log("route",route.state,route.token_type);
+        
     }
+
+
     var routeHref = window.location.href;
     
     const pathNameSplit = window.location.pathname.split('/');
