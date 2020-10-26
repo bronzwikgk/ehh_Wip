a9c722e642634391b3a489e69645d29df2b2932d
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-10-26 15:50:55 +0530

    u

--- js/ehhDataGrouping.js
+++ js/ehhDataGrouping.js
@@ -1,2 +1,3 @@
 //This script files contains function to sort, group, filter an array /json.
 //https://nikhilvijayan.com/group-array-objects-in-javascript
+//https://exploringjs.com/es6/ch_maps-sets.html
--- js/ehhServerNode.js
+++ js/ehhServerNode.js
@@ -2,3 +2,4 @@
 //https://jeffreyeverhart.com/2017/08/12/creating-crud-web-app-google-sheets/
 //https://www.bpwebs.com/how-to-create-a-data-entry-form-in-google-sheet/
 //https://www.bpwebs.com/how-to-create-an-online-data-entry-form-that-can-perform-crud-operations-on-google-sheets/
+//https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
