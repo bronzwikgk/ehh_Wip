a9a375907aa89c2abf4d69265e0a045ddc769bc8
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-10-26 14:57:57 +0530

    u

---
+++ js/ehhRuleengine.js
@@ -0,0 +1,6 @@
+//https://rete.js.org/#/
+//https://github.com/retejs/rete
+//http://product-configuration.hekkiez.nl/
+//http://jubyrajan.blogspot.com/2010/04/implementing-simple-deterministic-rule.html?m=0
+//http://noolsjs.com/
+//
---
+++ js/ehhServerNode.js
@@ -0,0 +1,4 @@
+//https://script.google.com/a/0dot1.live/d/1KwpoLic4yh83y1ULO8Q6wdJpGuHhyX9HnKudglSa7gfRIi-UMjhXJayP/edit?usp=sharing
+//https://jeffreyeverhart.com/2017/08/12/creating-crud-web-app-google-sheets/
+//https://www.bpwebs.com/how-to-create-a-data-entry-form-in-google-sheet/
+//https://www.bpwebs.com/how-to-create-an-online-data-entry-form-that-can-perform-crud-operations-on-google-sheets/
