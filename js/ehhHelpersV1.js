ee7ec8a29129e949f2b7db195dce52a8407a2081
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-11-06 00:39:03 +0530

    u

--- js/ehhHelpersV1.js
+++ js/ehhHelpersV1.js
@@ -1,5 +1,22 @@
 //helper Functions
 {
+    
+// var validjsontext = '{"firstnam":"James","surname":"Bond","mobile":["007-700-007","001-007-007-0007"]}'; 
+// var invalidjsontext = '{"firstnam""James","surname":"Bond","mobile":["007-700-007","001-007-007-0007"]}'; 
+// console.log("With Valid JSON Text: "+IsValidJSONString(validjsontext));
+// console.log("With inValid JSON Text: "+IsValidJSONString(invalidjsontext));
+function IsValidJSONString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+
+
+
     function hasChild(entity) {
         // console.log("iterating", entity, checkEntity(entity));//, entity.children.length
         if (entity.childNodes) {
@@ -99,4 +116,8 @@ function FindInAn2DArray(Array, column, item) {
 
 
     
+
+
 }
+
+
