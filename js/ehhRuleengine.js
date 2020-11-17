5d0f21e298ce1f94dca590220a49e2bd66a57515
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-11-17 23:25:26 +0530

    u

--- js/ehhRuleengine.js
+++
@@ -1,6 +0,0 @@
-//https://rete.js.org/#/
-//https://github.com/retejs/rete
-//http://product-configuration.hekkiez.nl/
-//http://jubyrajan.blogspot.com/2010/04/implementing-simple-deterministic-rule.html?m=0
-//http://noolsjs.com/
-//
--- js/ehhServerNode.js
+++
@@ -1,5 +0,0 @@
-//https://script.google.com/a/0dot1.live/d/1KwpoLic4yh83y1ULO8Q6wdJpGuHhyX9HnKudglSa7gfRIi-UMjhXJayP/edit?usp=sharing
-//https://jeffreyeverhart.com/2017/08/12/creating-crud-web-app-google-sheets/
-//https://www.bpwebs.com/how-to-create-a-data-entry-form-in-google-sheet/
-//https://www.bpwebs.com/how-to-create-an-online-data-entry-form-that-can-perform-crud-operations-on-google-sheets/
-//https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
